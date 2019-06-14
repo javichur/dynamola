9893f7893f77ec4be6c4cd002b32a616d492fba2
Javier Campos <javichur@gmail.com>
2019-06-14 20:34:49 +0200

    Added: getAllItemsByPartitionKey(partitionKeyValue), getItemsByPartitionKeyInRange(partitionKeyValue, rangeFrom, rangeTo), static createTableBasicWithSortKey(tableName) + lint.

--- index.js
+++ index.js
@@ -57,13 +57,80 @@ class Dynamola {
   }
 
   /**
-   * Obtiene un elemento en una tabla con Clave Partición (Clave Principal Simple)
+   * Obtiene un elemento en una tabla con Clave Principal Simple (solo Clave Partición)
    * @param {string} partitionKeyValue valor de la clave de partición.
    * @returns {Promise<Object>} promise con el elemento.
    */
   getItem(partitionKeyValue) {
     return this.getItemWithPrimarySortKey(partitionKeyValue, null);
   }
+
+
+  /**
+   * En tablas con Clave Principal Compuesta (partición+ordenación), devuelve todos los items para
+   * el valor de Clave de Partición dado.
+   * @param {string} partitionKeyValue valor de la clave de partición.
+   * @returns {Promise<Object>} promise con array de elementos.
+   */
+  getAllItemsByPartitionKey(partitionKeyValue) {
+    return new Promise((resolve, reject) => {
+      const params = {
+        TableName: this.tableName,
+        KeyConditionExpression: '#partitionKey = :val',
+        ExpressionAttributeNames: {
+          '#partitionKey': this.partitionKeyName,
+        },
+        ExpressionAttributeValues: {
+          ':val': partitionKeyValue,
+        },
+      };
+
+      this.docClient.query(params, (err, data) => {
+        if (err) {
+          this.customConsoleError('Unable to read items. Error JSON:', err);
+          return reject(JSON.stringify(err, null, 2));
+        }
+        this.customConsoleLog('GetItems succeeded:', data);
+        return resolve(data.Items);
+      });
+    });
+  }
+
+
+  /**
+   * En tablas con Clave Principal Compuesta (partición+ordenación), devuelve todos los items para
+   * el valor de Clave de Partición dado, cuya Clave de Ordenación está en el rango dado.
+   * @param {string} partitionKeyValue valor de la clave de partición.
+   * @param {string} rangeFrom inicio rango de la clave de ordenación.
+   * @param {string} rangeTo fin rango de la clave de ordenación.
+   */
+  getItemsByPartitionKeyInRange(partitionKeyValue, rangeFrom, rangeTo) {
+    return new Promise((resolve, reject) => {
+      const params = {
+        TableName: this.tableName,
+        KeyConditionExpression: '#partitionKey = :val and #sortKey between :from and :to',
+        ExpressionAttributeNames: {
+          '#partitionKey': this.partitionKeyName,
+          '#sortKey': this.sortKeyName,
+        },
+        ExpressionAttributeValues: {
+          ':val': partitionKeyValue,
+          ':from': rangeFrom,
+          ':to': rangeTo,
+        },
+      };
+
+      this.docClient.query(params, (err, data) => {
+        if (err) {
+          this.customConsoleError('Unable to read items. Error JSON:', err);
+          return reject(JSON.stringify(err, null, 2));
+        }
+        this.customConsoleLog('GetItems in range succeeded:', data);
+        return resolve(data.Items);
+      });
+    });
+  }
+
 
   /**
    * Añade un elemento a la tabla, con una clave de partición + clave de ordenación y
@@ -93,7 +160,7 @@ class Dynamola {
           this.customConsoleError('Unable to insert:', err);
           return reject(err);
         }
-        // The ReturnValues parameter is used by several DynamoDB operations; 
+        // The ReturnValues parameter is used by several DynamoDB operations;
         // however, PutItem (and put) does not recognize any values other than NONE or ALL_OLD.
         this.customConsoleLog('Saved Data. ', data);
         return resolve(params.Item); // devuelve input
@@ -154,7 +221,7 @@ class Dynamola {
   /**
    * Actualiza un elemento a la tabla, con una clave de partición + clave de ordenación, y
    * listado de atributos-valores que se actualizarán.
-   * 
+   *
    * No funciona con espacios en los nombres de los atributos.
    *
    * @param {string} partitionKeyValue valor de la clave de partición del elemento a actualizar.
@@ -166,7 +233,7 @@ class Dynamola {
     return new Promise((resolve, reject) => {
       const params = {
         TableName: this.tableName,
-        ReturnValues: 'UPDATED_NEW'
+        ReturnValues: 'UPDATED_NEW',
       };
 
       params.Key = this.createKey(partitionKeyValue, sortKeyValue);
@@ -175,7 +242,7 @@ class Dynamola {
       params.ExpressionAttributeValues = {};
       for (let i = 0; i < Object.keys(itemAttributesToChange).length; i += 1) {
         const name = Object.keys(itemAttributesToChange)[i];
-        //const nameNorm = `${name.replace(/\s/g, '_')}_value`; // no es suficiente
+        // const nameNorm = `${name.replace(/\s/g, '_')}_value`; // no es suficiente
 
         strUpdateExpression += ` ${name} = :${name}_value`;
         params.ExpressionAttributeValues[`:${name}_value`] = itemAttributesToChange[name];
@@ -222,8 +289,8 @@ class Dynamola {
         ReturnValues: 'UPDATED_NEW',
         UpdateExpression: `set ${attributeName} = ${attributeName} + :inc`,
         ExpressionAttributeValues: {
-          ":inc": increment
-        }
+          ':inc': increment,
+        },
       };
 
       params.Key = this.createKey(partitionKeyValue, sortKeyValue);
@@ -249,9 +316,9 @@ class Dynamola {
    */
   incrementCounter(partitionKeyValue, attributeName, increment) {
     return this.incrementCounterWithPrimarySortKey(partitionKeyValue,
-                                                   null,
-                                                   attributeName,
-                                                   increment);
+      null,
+      attributeName,
+      increment);
   }
 
 
@@ -286,6 +353,54 @@ class Dynamola {
           {
             AttributeName: 'Key',
             KeyType: 'HASH',
+          },
+        ],
+        ProvisionedThroughput: {
+          ReadCapacityUnits: 5,
+          WriteCapacityUnits: 5,
+        },
+        TableName: tableName,
+      };
+
+      const dynamodb = new AWS.DynamoDB();
+      dynamodb.createTable(params, (err, data) => {
+        if (err) {
+          return reject(err);
+        }
+        return resolve(data);
+      });
+    });
+  }
+
+  /**
+   * Crea una tabla dynamodb básica, con:
+   * - una clave de partición con nombre "Key" y tipo string.
+   * - una clave de ordenación con nombre "KeySort" y tipo string.
+   * - con capacidad aprovisionada de 5 lecturas y 5 escrituras.
+   * - sin índices secundarios.
+   * @param {string} tableName nombre de la tabla
+   */
+  static createTableBasicWithSortKey(tableName) {
+    return new Promise((resolve, reject) => {
+      const params = {
+        AttributeDefinitions: [
+          {
+            AttributeName: 'Key',
+            AttributeType: 'S',
+          },
+          {
+            AttributeName: 'SortKey',
+            AttributeType: 'S',
+          },
+        ],
+        KeySchema: [
+          {
+            AttributeName: 'Key',
+            KeyType: 'HASH',
+          },
+          {
+            AttributeName: 'SortKey',
+            KeyType: 'RANGE',
           },
         ],
         ProvisionedThroughput: {
